8720cc3fd4f7de88b8575114bc7e2b34aea9e84b
David Konsumer <konsumer@jetboystudio.com>
2020-11-07 00:27:43 -0800

    better setup

--- src/setup.js
+++ src/setup.js
@@ -1,5 +1,5 @@
 import { promises as fs } from 'fs'
-import { pads, controls } from './BeatStep'
+import { controls } from './BeatStep'
 
 // set initial state of controller
 async function setup (beatstep) {
@@ -8,25 +8,6 @@ async function setup (beatstep) {
   await beatstep.mode(controls.SHIFT, 'NOTE')
   await beatstep.noteChannel(controls.SHIFT, 0x0F)
   await beatstep.note(controls.SHIFT, 0x02)
-
-  // // TODO: can I force controller into CNTRL mode?
-
-  // for (const p in pads) {
-  //   await beatstep.mode(pads[p], 'NOTE')
-  //   await beatstep.noteChannel(pads[p], 0x01)
-  //   await beatstep.note(pads[p], 0x70 + p)
-
-  //   // this makes play stop working (so I can control it)
-  //   await beatstep.set(0x50, p, 0x01)
-  // }
-
-  // // TODO: is it possible to bind the level/rate knob?
-  // const controlsv = Object.values(controls)
-  // for (const c in controlsv) {
-  //   await beatstep.mode(controlsv[c], 'NOTE')
-  //   await beatstep.noteChannel(controlsv[c], 0x02)
-  //   await beatstep.note(controlsv[c], controlsv[c])
-  // }
 }
 
 export default setup
